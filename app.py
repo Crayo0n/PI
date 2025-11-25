2818c3682a29d7310ad04fb33552856f097f69fc
LuisEduardoSantanoDelgado <123046272@upq.edu.mx>
2025-11-24 22:02:17 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-#Funcionales
+# Funcionales
 from flask import Flask
-#Base de datos
+# Base de datos
 from db import db
 from config import Config
-#Rutas
+# Rutas
 from controllers.login import login_bp
 from controllers.usuarios import usuarios_bps
 from controllers.actividades import actividades_bps
 from controllers.rutinas import rutinas_bp
 from controllers.tareas import tareas_bp
+from controllers.usuarios.actualizarUsuario import actulizarUsuario_bp  # 👈 IMPORT AQUÍ
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -19,16 +20,14 @@ db.init_app(app)
 def create_tables():
     db.create_all()
 
-# Rutas -------------------------
 
 app.register_blueprint(login_bp)
 app.register_blueprint(rutinas_bp)
 app.register_blueprint(tareas_bp)
+app.register_blueprint(actulizarUsuario_bp)   # 👈 AQUÍ VA ESTE
 
 for bp in usuarios_bps + actividades_bps:
     app.register_blueprint(bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
